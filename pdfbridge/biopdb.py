e922aeeec9d5f61c302a6a4b081dacac24ed1c90
Toshiyuki HIRANO <hiracchi@users.noreply.github.com>
2016-12-04 23:57:03 +0900

    Fix: file close

--- pdfbridge/biopdb.py
+++ pdfbridge/biopdb.py
@@ -150,137 +150,137 @@ class Pdb(object):
         chain_serial = 0
         self._data.setdefault(model_serial, [])
 
-        fin = open(file_path, 'r')
-        while True:
-            line = fin.readline()
-            if (len(line) == 0):
-                break
-            line = line.rstrip('\n')
+        with open(file_path, 'r') as fin:
+            while True:
+                line = fin.readline()
+                if (len(line) == 0):
+                    break
+                line = line.rstrip('\n')
 
-            #if (len(line) != 80):
-            #    continue
-            if (self.debug == True):
-                print(line)
+                #if (len(line) != 80):
+                #    continue
+                if (self.debug == True):
+                    print(line)
 
-            record_name = line[0:6]
-            if record_name == 'SSBOND':
-                serNum = line[7:10]
-                chainID1 = str(line[15])
-                seqNum1 = int(line[17:21])
-                icode1 = str(line[21])
-                chainID2 = str(line[29])
-                seqNum2 = int(line[31:35])
-                icode2 = str(line[35])
+                record_name = line[0:6]
+                if record_name == 'SSBOND':
+                    serNum = line[7:10]
+                    chainID1 = str(line[15])
+                    seqNum1 = int(line[17:21])
+                    icode1 = str(line[21])
+                    chainID2 = str(line[29])
+                    seqNum2 = int(line[31:35])
+                    icode2 = str(line[35])
 
-                ssbond = ({'chain_id': chainID1,
-                           'seq_num': seqNum1},
-                          {'chain_id': chainID2,
-                           'seq_num': seqNum2})
-                self._ssbonds.append(ssbond)
+                    ssbond = ({'chain_id': chainID1,
+                               'seq_num': seqNum1},
+                              {'chain_id': chainID2,
+                               'seq_num': seqNum2})
+                    self._ssbonds.append(ssbond)
                 
-            elif ((record_name == 'ATOM  ') or (record_name == 'HETATM')):
+                elif ((record_name == 'ATOM  ') or (record_name == 'HETATM')):
 
-                if (len(line) < 80):
-                    line = line + (' ' * (80 - len(line)))
+                    if (len(line) < 80):
+                        line = line + (' ' * (80 - len(line)))
 
-                serial = int(line[6:11])
-                name = line[12:16]
-                alt_loc = line[16]
-                res_name = line[17:20]
-                chain_id = line[21]
-                if (chain_id == ' ') and (res_name != 'WAT'):
-                    chain_id = chr(ord('A') + (chain_serial % 26))
-                res_seq = line[22:26]
-                i_code = line[26]
-                coord_x = line[30:38]
-                coord_y = line[38:46]
-                coord_z = line[46:54]
-                occupancy = line[54:60].strip()
-                temp_factor = line[60:66].strip()
-                element = line[76:78].strip()
-                charge = line[78:80].strip()
+                    serial = int(line[6:11])
+                    name = line[12:16]
+                    alt_loc = line[16]
+                    res_name = line[17:20]
+                    chain_id = line[21]
+                    if (chain_id == ' ') and (res_name != 'WAT'):
+                        chain_id = chr(ord('A') + (chain_serial % 26))
+                    res_seq = line[22:26]
+                    i_code = line[26]
+                    coord_x = line[30:38]
+                    coord_y = line[38:46]
+                    coord_z = line[46:54]
+                    occupancy = line[54:60].strip()
+                    temp_factor = line[60:66].strip()
+                    element = line[76:78].strip()
+                    charge = line[78:80].strip()
 
-                item = {}
-                item['serial'] = serial
-                item['record_name'] = record_name
-                item['name'] = name
-                item['alt_loc'] = alt_loc
-                item['res_name'] = res_name
-                item['chain_id'] = chain_id
-                item['res_seq'] = int(res_seq)
-                item['i_code'] = i_code
-                item['coord'] = [float(coord_x), float(coord_y), float(coord_z)]
+                    item = {}
+                    item['serial'] = serial
+                    item['record_name'] = record_name
+                    item['name'] = name
+                    item['alt_loc'] = alt_loc
+                    item['res_name'] = res_name
+                    item['chain_id'] = chain_id
+                    item['res_seq'] = int(res_seq)
+                    item['i_code'] = i_code
+                    item['coord'] = [float(coord_x), float(coord_y), float(coord_z)]
                 
-                if (len(occupancy) != 0):
-                    item['occupancy'] = float(occupancy)
-                else:
-                    item['occupancy'] = 1.0
-                if (len(temp_factor) != 0):
-                    item['temp_factor'] = float(temp_factor)
-                else:
-                    item['temp_factor'] = 0.0
-
-                if (len(element) != 0):
-                    # TODO: 原子変換テーブル作成
-                    element = element.strip()
-                    if element == 'D':
-                        element = 'H'
-                    item['element'] = element
-                else:
-                    # TODO: テーブルを持って変換するように変更
-                    name2 = name.lstrip().upper()
-
-                    if name2[0:2] == 'CL':
-                        element = 'Cl'
-                    elif name2[0:2] == 'NA':
-                        element = 'Na'
-                    elif name2[0:2] == 'MG':
-                        element = 'Mg'
-                    elif name2[0:2] == 'FE':
-                        element = 'Fe'
-                    elif name2[0] == 'H':
-                        element = 'H'
+                    if (len(occupancy) != 0):
+                        item['occupancy'] = float(occupancy)
                     else:
-                        element = name[1]
-                    item['element'] = element
+                        item['occupancy'] = 1.0
+                    if (len(temp_factor) != 0):
+                        item['temp_factor'] = float(temp_factor)
+                    else:
+                        item['temp_factor'] = 0.0
 
-                if (len(charge) != 0):
-                    charge_last_char = charge[-1]
-                    if (charge_last_char == '+') or (charge_last_char == '-'):
-                        charge = charge_last_char + charge[0:-1]
-                    item['charge'] = charge
-                else:
-                    item['charge'] = "  "
+                    if (len(element) != 0):
+                        # TODO: 原子変換テーブル作成
+                        element = element.strip()
+                        if element == 'D':
+                            element = 'H'
+                        item['element'] = element
+                    else:
+                        # TODO: テーブルを持って変換するように変更
+                        name2 = name.lstrip().upper()
 
-                self._data[model_serial].append(item)
-                continue
-            elif (record_name == 'MODEL '):
-                serial = int(line[10:14])
-                model_serial = serial
-                self._data.setdefault(model_serial, [])
-                chain_serial = 0
-                continue
-            elif (record_name == 'TER   '):
-                if (len(line) < 27):
-                    line = line + (' ' * (27 - len(line)))
-                serial = int(line[6:11]) if line[6:11].isdigit() else 0
-                resname = line[17:20]
-                chain_id = line[21]
-                if (chain_id == ' ') and (res_name != 'WAT'):
-                    chain_id = chr(ord('A') + (chain_serial % 26))
-                    chain_serial += 1
-                res_seq = line[22:26]
-                i_code = line[26]
-                item = {}
-                item['serial'] = serial
-                item['record_name'] = record_name
-                item['res_name'] = res_name
-                item['chain_id'] = chain_id
-                item['res_seq'] = res_seq
-                item['i_code'] = i_code
+                        if name2[0:2] == 'CL':
+                            element = 'Cl'
+                        elif name2[0:2] == 'NA':
+                            element = 'Na'
+                        elif name2[0:2] == 'MG':
+                            element = 'Mg'
+                        elif name2[0:2] == 'FE':
+                            element = 'Fe'
+                        elif name2[0] == 'H':
+                            element = 'H'
+                        else:
+                            element = name[1]
+                        item['element'] = element
 
-                self._data[model_serial].append(item)
-                continue
+                    if (len(charge) != 0):
+                        charge_last_char = charge[-1]
+                        if (charge_last_char == '+') or (charge_last_char == '-'):
+                            charge = charge_last_char + charge[0:-1]
+                        item['charge'] = charge
+                    else:
+                        item['charge'] = "  "
+
+                    self._data[model_serial].append(item)
+                    continue
+                elif (record_name == 'MODEL '):
+                    serial = int(line[10:14])
+                    model_serial = serial
+                    self._data.setdefault(model_serial, [])
+                    chain_serial = 0
+                    continue
+                elif (record_name == 'TER   '):
+                    if (len(line) < 27):
+                        line = line + (' ' * (27 - len(line)))
+                    serial = int(line[6:11]) if line[6:11].isdigit() else 0
+                    resname = line[17:20]
+                    chain_id = line[21]
+                    if (chain_id == ' ') and (res_name != 'WAT'):
+                        chain_id = chr(ord('A') + (chain_serial % 26))
+                        chain_serial += 1
+                    res_seq = line[22:26]
+                    i_code = line[26]
+                    item = {}
+                    item['serial'] = serial
+                    item['record_name'] = record_name
+                    item['res_name'] = res_name
+                    item['chain_id'] = chain_id
+                    item['res_seq'] = res_seq
+                    item['i_code'] = i_code
+
+                    self._data[model_serial].append(item)
+                    continue
 
     def get_atomgroup(self, select_model=None, select_altloc='A'):
         """
