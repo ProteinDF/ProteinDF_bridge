f38a4eb8f7c894479b6a60ddac3cb26c473a714f
Toshiyuki HIRANO <hiracchi@users.noreply.github.com>
2019-05-23 15:22:38 +0900

    feat: brd and mpac I/O functions

--- proteindf_bridge/functions.py
+++ proteindf_bridge/functions.py
@@ -11,18 +11,33 @@ except:
 
 from .atomgroup import AtomGroup
 
-def load_atomgroup(brd_file_path):
-    mpac_file = open(brd_file_path, "rb")
-    mpac_data = msgpack.unpackb(mpac_file.read())
-    mpac_file.close()
+
+def load_msgpack(mpac_path):
+    """load message pack file
+    """
+    mpac_data = None
+    with open(mpac_path, "rb") as f:
+        mpac_data = msgpack.unpackb(f.read())
+
+    return mpac_data
+
+def save_msgpack(mpac_path, data):
+    mpac_data = msgpack.packb(data)
+    with open(mpac_path, "wb") as f:
+        f.write(mpac_data)
+
+
+def load_atomgroup(brd_path):
+    """load bridge file (msgpack format)
+    """
+    mpac_data = load_msgpack(brd_path)
     atomgroup = AtomGroup(mpac_data)
 
     return atomgroup
 
+
 def save_atomgroup(atomgroup, file_path):
+    """save bridge file (msgpack format)
+    """
     data = atomgroup.get_raw_data()
-    mpac = msgpack.packb(data)
-
-    with open(file_path, "wb") as f:
-        f.write(mpac)
-
+    save_msgpack(file_path, data)
